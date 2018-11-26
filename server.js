3bb3b30340bdd12b7b2a857498e686d67f8b6997
Nick Dambrosio <ndambro303@gmail.com>
2018-11-26 15:13:01 -0700

    we made a server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
-
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
+app.locals.title = "Juicy";
+
+app.set("port", process.env.PORT || 3000);
+
 app.listen(app.get("port"), () => {
-  console.log(`${app.locals.title} is running on ${app.get("port")}`);
+  console.log(`${app.locals.title} is running ${app.get("port")}`);
 });
