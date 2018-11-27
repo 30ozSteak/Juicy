6cd97fbec26b5d1e3fa0782331a4a44ebb694468
Nick Dambrosio <ndambro303@gmail.com>
2018-11-26 20:26:36 -0700

    add random color generator

---
+++ public/scripts/colorGen.js
@@ -0,0 +1,11 @@
+const makeColor = () => {
+  return (
+    "rgb(" +
+    Math.floor(Math.random() * 256) +
+    ", " +
+    Math.floor(Math.random() * 256) +
+    ", " +
+    Math.floor(Math.random() * 256) +
+    ")"
+  );
+};
