d836cbeb285eba546576cfea3e619d7c242ed67d
Nick Dambrosio <ndambro303@gmail.com>
2018-11-29 18:44:24 -0700

    cleaned up UI, added onboarding experience

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,23 +1,45 @@
+
 const colors = document.querySelectorAll(".color");
 const shuffle = document.querySelector(".shuffle");
-const magicButton = document.querySelector('.magic-button')
+const magicButton = document.querySelector('.magic-button');
+const showSaveBtn = document.querySelector('.d-m-btn')
+const menuButton = document.querySelector('.🍔');
+const infoButton = document.querySelector('.ℹ️')
+
 
 
 magicButton.addEventListener('click', displayColors);
 shuffle.addEventListener("click", genColors);
+showSaveBtn.addEventListener('click', openSaveMenu);
+menuButton.addEventListener('click', openProjectMenu);
+infoButton.addEventListener('click', showInfoMenu);
 
+function openProjectMenu() {
+  let projectMenu = document.querySelector('.project-menu');
+  let openBurger = document.querySelector('.🍔');
+  openBurger.classList.toggle('🍔-expanded')
+  projectMenu.classList.toggle('p-m-active')
+}
+
+function openSaveMenu() {
+  let saveBlock = document.querySelector('.save-block');
+  showSaveBtn.classList.toggle('d-m-btn-active')
+  saveBlock.classList.toggle('save-block-open')
+}
+
+function showInfoMenu() {
+  let infoBlock = document.querySelector('.info-overlay');
+  infoBlock.classList.toggle('info-overlay-shown')
+}
 
 function genColors() {
   for (let i = 0; i < 5; i++) {
     colors[i].style.backgroundColor = new ColoredBox().blockColor;
-    console.log(colors);
   }
 }
 
 function displayColors() {
   magicButton.classList.toggle('open');
-  console.log('y');
-  // magicButton.classList.toggle('open-button');
 }
 
 class ColoredBox {
@@ -29,3 +51,6 @@ class ColoredBox {
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 }
+
+
+
