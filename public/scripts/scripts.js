af2ea42f0afb38c94aef49bfedd6b8b37aef510c
Nick Dambrosio <ndambro303@gmail.com>
2018-12-01 12:25:51 -0700

    working on fetch calls

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,18 +1,45 @@
-
 const colors = document.querySelectorAll(".color");
 const shuffle = document.querySelector(".shuffle");
 const magicButton = document.querySelector('.magic-button');
 const showSaveBtn = document.querySelector('.d-m-btn')
 const menuButton = document.querySelector('.🍔');
 const infoButton = document.querySelector('.ℹ️')
-
-
+const colorBlock = document.querySelector('.color-block');
+const saveColorsButton = document.querySelector('.save-color-palette-btn')
+const saveColors = document.querySelector('.save-color');
+const paletteInput = document.querySelector('.save-color-palette-input');
+const saveProjectInput = document.querySelector('.save-project-input');
+const saveProjectButton = document.querySelector('.submit-project-button');
 
 magicButton.addEventListener('click', displayColors);
+colorBlock.addEventListener('click', lockColor);
+saveProjectButton.addEventListener('click', saveProject)
 shuffle.addEventListener("click", genColors);
 showSaveBtn.addEventListener('click', openSaveMenu);
 menuButton.addEventListener('click', openProjectMenu);
 infoButton.addEventListener('click', showInfoMenu);
+saveColorsButton.addEventListener('click', savePalette)
+
+function saveProject(ev) {
+  ev.preventDefault();
+  let projectName = saveProjectInput.value;
+  postProjects(projectName);
+}
+
+function savePalette(ev) {
+  ev.preventDefault();
+  let paletteName = paletteInput.value;
+  let savedPalette = {
+    name: paletteName,
+    proj_id: 'lol',
+    color_1: colors[0].id,
+    color_2: colors[1].id,
+    color_3: colors[2].id,
+    color_4: colors[3].id,
+    color_5: colors[4].id,
+  }
+  console.log(savedPalette);
+}
 
 function openProjectMenu() {
   let projectMenu = document.querySelector('.project-menu');
@@ -34,9 +61,17 @@ function showInfoMenu() {
 
 function genColors() {
   for (let i = 0; i < 5; i++) {
-    colors[i].style.backgroundColor = new ColoredBox().blockColor;
+    let hex = new ColoredBox().blockColor;
+    colors[i].style.backgroundColor = hex
+    colors[i].id = hex
   }
 }
+
+function lockColor(ev) {
+  if (ev.target.classList.contains('color')) {
+    ev.target.classList.toggle('locked')
+  }
+};
 
 function displayColors() {
   magicButton.classList.toggle('open');
@@ -52,5 +87,19 @@ class ColoredBox {
   }
 }
 
-
-
+function postProjects(name) {
+  fetch("/api/v1/projects", {
+      method: 'POST',
+      body: JSON.stringify({
+        name
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(console.log)
+    .catch(err => console.log(err))
+}
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const env = process.env.NODE_ENV || "development";
-// const configure = require("./knexfile")[env];
-// const database = require("knex")(configure);
+const configuration = require('./knexfile')[env];
+const database = require('knex')(configuration);
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -14,6 +12,49 @@ app.locals.title = "Juicy";
 
 app.set("port", process.env.PORT || 3000);
 
-app.listen(app.get("port"), () => {
-  console.log(`JUICYYYYYYY is running on http://localhost:${app.get("port")}`);
+app.get("/api/v1/projects", (request, response) => {
+  database('projects').select()
+    // knex is saying select all in projects from the db
+    .then((projects) => {
+      response.status(200).json(projects)
+    })
+    .catch((error) => {
+      response.status(500).json({
+        error
+      })
+    })
 });
+
+app.post("/api/v1/projects", (request, response) => {
+  const project = request.body;
+  console.log(project);
+  if (!project.name) {
+    return response
+      .status(422)
+      .send({
+        error: `You need a name property 💔`
+      })
+  } else {
+    database('projects').insert(project, 'id')
+      .then(project => {
+        response.status(201).json({
+          id: project[0]
+        })
+      })
+      .catch(error => {
+        response.status(500).json({
+          error
+        })
+      })
+  }
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`JUICYYYYYYY is running on https://localhost:${app.get("port")}`);
+});
+
+// get('/api/v1/projects/:project_id/palettes')
+// post("/api/v1/:project_id/palettes", );
+
+// delete("/api/v1/project/:project_id/palettes/:palette_id", );
+// delete("/api/v1/projects/:project_id", );
